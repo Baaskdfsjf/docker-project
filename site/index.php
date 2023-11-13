ef14309f5df07ba712c2062dae9f4bd801d12ce9
Moan Lisa <mona@lisa.com>
2023-11-13 11:34:38 +0100

    edit code 1

--- site/index.php
+++ site/index.php
@@ -1,5 +1,18 @@
-<?php
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mijn eerste project</title>
+</head>
+<body>
+    <?php
+    $tvShow = "The simpons";
 
-echo "I am a big fan of the simpons"
+    $tvshow = "Family Guy";
+    echo "<h1>$tvshow</h1>";
 
-?>
+
+    ?>
+</body>
+</html>
+
